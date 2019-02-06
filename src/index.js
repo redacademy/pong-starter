ac3588e2487e094dc9cd4bde9de0afc748deef24
Mandi Wise <hello@mandiwise.com>
2019-02-06 13:07:15 -0800

    Update to WP4 and fix eslint config.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import './styles/game.css';
-import Game from './partials/Game'
+import Game from './partials/Game';
 
 // create a game instance
 const game = new Game('game', 512, 256);
 
 (function gameLoop() {
-    game.render();
-    requestAnimationFrame(gameLoop);
+  game.render();
+  requestAnimationFrame(gameLoop);
 })();
--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,63 @@
+
 const webpack = require('webpack');
 const path = require('path');
-const resolve = require('path').resolve;
+const resolve = path.resolve;
 const src = resolve(__dirname, 'src');
 const build = resolve(__dirname, 'build');
-const OpenBrowserPlugin = require('open-browser-webpack-plugin');
 
 module.exports = {
-	entry: {
-		app: './src/index.js',
-	},
+  mode: 'none',
 
-	output: {
+  entry: {
+    app: './src/index.js'
+  },
+
+  output: {
 		path: build,
-		filename: 'bundle.js',
-	},
+		filename: 'bundle.js'
+  },
 
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				include: [src],
-				exclude: /node_modules/,
-				use: [
-					'style-loader',
-					'css-loader'
-				]
-			},
-			{
-				test: /\.js$/,
-				enforce: 'pre',
-				loader: 'eslint-loader?configFile=.eslintrc',
-				include: [src],
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.js$/,
-				loader: 'babel-loader',
-				query: {
-					presets: ['env'],
-				},
-				include: [src],
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.(eot|svg|ttf|woff|woff2)$/,
-				loader: 'file-loader?name=public/fonts/[name].[ext]&emitFile=false'
-			}
-		]
-	},
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        include: [src],
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        loader: 'eslint-loader?configFile=.eslintrc',
+        include: [src],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env']
+        },
+        include: [src],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=public/fonts/[name].[ext]&emitFile=false'
+      }
+    ]
+  },
 
-	devServer: {
-		historyApiFallback: true,
-		host: '0.0.0.0',
-		port: 3000,
-		stats: 'errors-only',
-		publicPath: '/build/',
-		watchOptions: {
-			aggregateTimeout: 300,
-			poll: 1000,
-			ignored: /node_modules/
-		}
-	},
-
-	plugins: [
-		new OpenBrowserPlugin({ url: 'http://localhost:3000/' }),
-	]
+  devServer: {
+		contentBase: path.resolve(__dirname, './'),
+    watchContentBase: true,
+    historyApiFallback: true,
+    open: true,
+    port: 3000,
+    publicPath: '/build/',
+    stats: 'errors-only',
+    watchOptions: {
+      ignored: /node_modules/
+    }
+  }
 };
