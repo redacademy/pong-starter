f320e4711e2be7316a0786463d142aac8d925839
robhyrk <rob.hyrkiel@gmail.com>
2019-10-16 09:43:53 -0400

    feat: add files, created svg canvas

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,13 +1,23 @@
+import {SVG_NS} from '../settings'
+
 export default class Game {
   constructor(element, width, height) {
     this.element = element;
     this.width = width;
     this.height = height;
 
+    this.gameElement = document.getElementById(this.element)
+
 		// Other code goes here...
   }
 
   render() {
-		// More code goes here....
+    let svg = document.createElementNS(SVG_NS, 'svg')
+    
+    svg.setAttributeNS(null, 'width', this.width)
+    svg.setAttributeNS(null, 'height', this.height)
+    svg.setAttributeNS(null, 'viewBox', `0 0 ${this.width} ${this.height}`)
+    this.gameElement.appendChild(svg)
+
   }
 }
